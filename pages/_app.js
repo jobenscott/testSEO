9b077f76b3ddd7879f43c104923ca6bca72bfb33
Aaron Eveleth <aaroneveleth@Aarons-MacBook-Pro.local>
2022-12-18 01:22:16 -0700

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,16 @@ const MyApp = ({ Component, pageProps }) => {
         <link
           rel="shortcut icon"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L9RNM1TM9B"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L9RNM1TM9B');
+          `}
+        </script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
