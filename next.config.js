a2b5020d8ef03e1d3b4da88f9afcb071e48d5d2a
Aaron Eveleth <aaroneveleth@Aarons-MacBook-Pro.local>
2022-12-22 02:30:23 -0500

    i hate htis

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
 
   // adding this fixes it for me:
   swcMinify: false,
+  trailingSlash: true,
 }
 // module.exports = (phase, nextConfig) => {
 //   return withBundleAnalyzer(nextConfig)
