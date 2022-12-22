2e64c1d7a80e8da1be84d1af4dd50fa33edda554
Aaron Eveleth <aaroneveleth@Aarons-MacBook-Pro.local>
2022-12-22 02:16:43 -0500

    amything

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer');
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
-  api: {
-    // disables call to body parsing module
-    bodyParser: false,
-  },
+  // api: {
+  //   // disables call to body parsing module
+  //   bodyParser: false,
+  // },
   images: {
     loader: "default",
     domains: ["localhost", "30d1-2600-8800-620f-f600-fd42-d640-7a4d-da7b.ngrok.io"],
